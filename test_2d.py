ae4c44f5b99fc5be5220a833ee9525698588b668
Kaeryv <kaeryv.alden@gmail.com>
2024-05-02 10:14:49 +0200

    Fixed example

--- test_2d.py
+++ test_2d.py
@@ -3,6 +3,7 @@ from aurora.misc import kmatrices, kmatrices
 import numpy as np
 from scipy.linalg import eig
 from math import pi
+import matplotlib.pyplot as plt
 N = 301
 x = np.arange(-N//2+1, N//2+1)
 X, Y = np.meshgrid(x, x)
@@ -14,7 +15,7 @@ UC = convmat(np.ones((N, N, 1)), P, Q)
 omegasE= []
 omegasH= []
 for i, beta in enumerate(np.linspace(0, 0.5, 40)):
-    Kx, Ky, Kz = kmatrices([0, beta * 2 * pi / 1.0, 0], P, Q, R, 1.0)
+    Kx, Ky, Kz = kmatrices([0, beta * 2 * pi / 1.0, 0], P, Q, R, (1.0,1.0,0.0))
     # E mode
     A = Kx @ np.linalg.inv(UC) @ Kx  + Ky @ np.linalg.inv(UC) @ Ky
     w, v = eig(A, RC)
@@ -27,6 +28,14 @@ for i, beta in enumerate(np.linspace(0, 0.5, 40)):
     k0 = w
     omega = np.real(np.sqrt(k0) / 2 / pi)
     omegasH.append(omega)
+
+SZ = v[:, -1].reshape(7,7)
+EZ =  np.fft.ifft2(np.fft.ifftshift(np.pad(SZ,63)))
+extent = [0,2,0,2]
+plt.matshow(np.tile(np.real(EZ), (2,2)), cmap="bwr", extent=extent)
+plt.contour(np.tile(np.real(struct[...,0]), (2,2)), color="k",extent=extent)
+plt.axis("equal")
+plt.show()
 
 omegasE = np.asarray(omegasE)
 omegasH = np.asarray(omegasH)
